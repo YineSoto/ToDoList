4bbfd9bb4877c563319f9fbea5de7510b8dc2dd0
Yinesa Soto <yinesasoto@gmail.com>
2024-06-24 19:33:08 +0200

    Nuevos cambios

--- TodoApi/Models/TodoContext.cs
+++ TodoApi/Models/TodoContext.cs
@@ -6,12 +6,12 @@ public class TodoContext : DbContext
 {
 
     public TodoContext(DbContextOptions<TodoContext> options)
-        : base(options)
+        : base(options) // es la super en Java
     {
     }
 
+    /*Conjunto o coleccion que contendrá los datos
+    El null tiene un signo de exclamacion afirmando que se va a recibir o 
+    ingresar un null*/
     public DbSet<TodoItem> TodoItems { get; set; } = null!;
-
-
-
 }
